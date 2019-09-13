a29a442bd412156152ccdb10a0ca0da8011df5ee
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-13 00:12:12 -0300

    criando componentes Button e Input

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import Background from '~/components/Background';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Background />;
+  return (
+    <Background>
+      <Input icon="call" placeholder="Digite seu nome" />
+      <Button>Entrar</Button>
+    </Background>
+  );
 }
