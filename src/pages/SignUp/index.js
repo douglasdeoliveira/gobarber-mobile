b75111e7fa81998f967ca4b31cb9ac15e35bd62d
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-13 00:36:54 -0300

    estilizando pagina SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Image } from 'react-native';
 
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SignLink,
+  SignLinkText,
+  SubmitButton,
+} from './styles';
 
-export default function SignUp() {
-  return <Background />;
+export default function SignUp({ navigation }) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu nome completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Fazer login</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 }
